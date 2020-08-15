f6d1a66f6193cdeed26d100b14c74e444cd626ac
kate <kan004@ucr.edu>
2020-08-15 16:15:50 -0700

    updated comments in food js models

--- models/food.js
+++ models/food.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../config/connection.js");
 
-// Creates a "Chirp" model that matches up with DB
+// Creates a "Restaurant" model that matches up with DB
 const Restaurant = sequelize.define("restaurant", {
   restaurant_name: Sequelize.STRING,
   cuisine: Sequelize.STRING,
@@ -13,5 +13,5 @@ const Restaurant = sequelize.define("restaurant", {
 // Syncs with DB
 Restaurant.sync();
 
-// Makes the Chirp Model available for other files (will also create a table)
+// Makes the Restaurant Model available for other files (will also create a table)
 module.exports = Restaurant;
