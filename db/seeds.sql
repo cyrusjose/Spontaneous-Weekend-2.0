f803b620d71b77410cdf57325913007697ecf154
kate <kan004@ucr.edu>
2020-08-15 15:46:25 -0700

    added restaurants seed sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,20 @@
+-- seed for parks
 INSERT INTO t_parks (trail_name, trail_location, difficulty, distance, trail_URL)
 VALUES ("mount rubidoux drive", "Riverside, California", "greenBlue", 2.4, "https://www.hikingproject.com/trail/7011084/mount-rubidoux-drive");
+
+-- seed for food
+INSERT INTO restaurants (restaurant_name, cuisine, price, address)
+VALUES ("Mcdonalds", "American", 1, "2242 University Ave, Riverside, CA 92507");
+
+INSERT INTO restaurants (restaurant_name, cuisine, price, address)
+VALUES ("Tasty Noodle House", "Taiwanese", 2, "15333 Culver Dr #320, Irvine, CA 92604");
+
+INSERT INTO restaurants (restaurant_name, cuisine, price, address)
+VALUES ("Din Tai Fung", "Taiwanese", 3, "177 Caruso Ave, Glendale, CA 91210");
+
+INSERT INTO restaurants (restaurant_name, cuisine, price, address)
+VALUES ("Outback", "American", 3, "25322 Cabot Rd, Laguna Hills, CA 92653");
+
+
+
+
