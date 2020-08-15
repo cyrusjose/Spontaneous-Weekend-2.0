e775d43268ed34521488b2a4c516f850e9255b45
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-15 08:26:02 -0700

    Changed functionality of navbar

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
   // Required for nav bar mobile functionality
   $(".sidenav").sidenav();
+
+  $(".hover").mouseover(() => {
+    $(".hover").addClass("active");
+  });
+  $(".hover").mouseout(() => {
+    $(".hover").removeClass("active");
+  });
 });
