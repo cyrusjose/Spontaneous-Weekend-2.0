8c43760f26ca93a7e208edec33edbd479b75f50c
Melinda Winter <melindawinter42@gmail.com>
2020-08-18 21:27:47 -0700

    sequelize init

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,2 @@
--- parks db and table
-CREATE DATABASE activities_db;
-
-use activities_db;
-
-CREATE TABLE t_parks (
-trail_name VARCHAR(255) NOT NULL,
-trail_location VARCHAR(255) NOT NULL,
-difficulty VARCHAR(255) NOT NULL,
-distance DECIMAL(5,2),
-trail_URL VARCHAR(255)
-);
--- restaurants table
-
-CREATE TABLE restaurants (
-  restaurant_id INT NOT NULL AUTO_INCREMENT,
-  restaurant_name varchar(60) NOT NULL,
-  cuisine VARCHAR(60),
-  price INT,
-  address VARCHAR(80),
-  PRIMARY KEY (restaurant_id)
-);
+CREATE DATABASE spontaneous_db;
+use spontaneous_db;
