e99fbd3fa604dd3856447f8cf90aa8424f55b606
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-18 19:26:19 -0700

    Added login route

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -2,16 +2,19 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/movies", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/movie.html"));
+  res.sendFile(path.join(__dirname, "../public/movie.html"));
 });
 router.get("/food", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/food.html"));
+  res.sendFile(path.join(__dirname, "../public/food.html"));
 });
 router.get("/park", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/park.html"));
+  res.sendFile(path.join(__dirname, "../public/park.html"));
+});
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
 module.exports = router;
